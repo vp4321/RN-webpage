e677e99fc77f64f8865dbde1cbb98b3457148f7d
Amool-kk <amoolkumar10@gmail.com>
2021-03-20 21:46:20 +0530

    finnal

--- RN website/index.js
+++ RN website/index.js
@@ -1,89 +1,108 @@
 console.log("welcome on this page")
 
 const modalbtn = document.querySelectorAll('.model-open')
-const closebtn = document.querySelectorAll('.modal-close');
+
 const modalcontent = document.querySelectorAll('.modal-content')
 
-function locationof(e) {
-    var x;
-    var y;
-    if (e.pageX || e.pageY) {
-        x = e.pageX;
-        y = e.pageY;
-    }
-    else {
-        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-    x -= gCanvasElement.offsetLeft;
-    y -= gCanvasElement.offsetTop;
-}
 
-// for (let i = 0; i < modalbtn.length; i++) {
-//     const btn = modalbtn[i];
-//     // console.log(btn)
-//     btn.addEventListener('click', () => {
-//         const modal = btn.getAttribute("data-modal")
-//         console.log(modalcontent[i].style.margin - top)
-//         document.getElementById(modal).style.display = "block";
-//     })
-// }
+//....................for modal fetch ..................
 
-for(let k = 0; k< modalbtn.length; k ++){
+for (let k = 0; k < modalbtn.length; k++) {
     const btn = modalbtn[k];
-    btn.addEventListener('click', ()=>{
+    btn.addEventListener('click', () => {
         const modal = btn.getAttribute("data-modal")
         const div = document.getElementById(modal)
-        fetch(`./modals/${modal}.html`).then(res=>res.text())
-        .then(data=>{
-            console.log(div)
-            console.log("here is done")
-            div.innerHTML = data;
-            div.style.display = "block";
-            history.pushState(null,null, `#${modal}`)
-        })
+        fetch(`./modals/${modal}.html`).then(res => res.text())
+            .then(data => {
+                // console.log(div)
+
+                // console.log("here is done")
+                div.innerHTML = data;
+                div.style.display = "block";
+                history.pushState(null, null, `#${modal}`)
+                // const closebtn = document.querySelectorAll('.modal-close')
+                const closebtn = document.getElementById(`trybtn${k}`)
+                console.log(closebtn,k)
+                closebtn.addEventListener('click',()=>{
+                    div.style.display = "none"
+                    // console.log('event fired')
+                })
+            })
     })
 }
 
 
-
+//....................... for close modal click on window ...................
 
 window.onclick = function (e) {
     if (e.target.className === 'modal') {
         e.target.style.display = "none";
-        history.pushState(null,null, "http://127.0.0.1:5500/RN-webpage/RN%20website/index.html")
+        history.pushState(null, null, "http://127.0.0.1:5500/RN-webpage/RN%20website/index.html")
     }
 }
 
 
 const navbar = document.querySelector('.navbar')
-console.log(navbar)
-window.onscroll = function () { scrollFunction() }
 
-function scrollFunction() {
-    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
-        // navbar.style.display = "none"
-        navbar.style.background = '#222'
-    } else {
-        // navbar.style.display = "block"
-        navbar.style.background = 'transparent'
-    }
-}
+//..................... for send email ....................
 
 const sendbtn = document.querySelector('.sendbtn')
 
-sendbtn.addEventListener('click',()=>{
+
+sendbtn.addEventListener('click', () => {
+    const body = "Name: " + document.getElementById('yourname').value + '<br>' + "Message: " + document.getElementById('yourmsg').value
     Email.send({
-        SecureToken : "eab6f597-d43b-44ef-914f-4a68130d92de",
+        SecureToken: "eab6f597-d43b-44ef-914f-4a68130d92de",
         To: "nitrozradio56@gmail.com",
         From: document.getElementById('youremail').value,
-        Subject : document.getElementById('yoursubject').value,
-        Body: document.getElementById('yourmsg').value 
-    }).then(msg=>alert("The email successfully sent"))
+        Subject: document.getElementById('yoursubject').value,
+        Body: body
+    }).then(msg =>{
+         alert("The email successfully sent")
+         document.getElementById('yourname').value = "";
+         document.getElementById('youremail').value = "";
+         document.getElementById('yoursubject').value = "";
+         document.getElementById('yourmsg').value = "";
+    })
 })
 
+
+//..................for navbar...........................
+
+
+const he = document.querySelector('.header').offsetHeight - 40
+const replace = document.querySelector('.replace')
+// const navbar = document.querySelector('.navbar')
+console.log(he)
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > he || document.documentElement.scrollTop > he) {
+        console.log("event fired")
+        navbar.classList.add('fixed')
+        navbar.style.background = '#222'
+        navbar.style.display = "block"
+        // navbar.style.padding = "0.2rem 0.5rem"
+        // replace.style.display = "block";
+    }
+    else {
+        navbar.classList.remove('fixed')
+        navbar.style.background = 'transparent'
+        navbar.style.display = "none"
+        // replace.style.display = "none"
+    }
+})
+
+
+// window.addEventListener('scroll',()=>{
+//     if(document.body.scrollTop > he || document.documentElement.scrollTop > he){
+//         console.log("event fired")
+//         navbar.classList.add('fixed')
+//     }
+//     else{
+//         navbar.classList.remove('fixed')
+//     }
+// })
 // function sendemail(e) {
-    
+
 //     Email.send({
 //         name: document.getElementById('yourname').value,
 //         Host : "smtp.gmail.com",
@@ -93,7 +112,7 @@ sendbtn.addEventListener('click',()=>{
 //         From : "nitrozradio56@gmail.com",
 //         Subject : document.getElementById('yoursubject').value,
 //         Body: document.getElementById('yourmsg').value,
-        
+
 //     }).then(function(response){
 //         console.log(response)
 //         if(response == 'OK'){
@@ -105,3 +124,15 @@ sendbtn.addEventListener('click',()=>{
 // }
 
 // 432605
+
+
+     //.................... for preloader ................................
+
+const preloader = document.getElementById('loading');
+
+function myFunction() {
+    setTimeout(() => {
+        preloader.style.display = "none"
+    }, 2000);
+
+}
